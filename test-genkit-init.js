c4ee8135362be97ed0e196568e0048da529d5f8f
Arshad Ali <munazaali960@gmail.com>
2025-12-08 15:12:51 +0000

    after downloading bubble wrap = 65

--- test-genkit-init.js
+++
@@ -1,6 +0,0 @@
-import { googleAI } from '@genkit-ai/googleai';
-
-console.log('Google AI plugin type:', typeof googleAI);
-const plugin = googleAI();
-console.log('Plugin created:', !!plugin);
-console.log('Plugin keys:', Object.keys(plugin));
--- test-genkit.js
+++
@@ -1,8 +0,0 @@
-import('@genkit-ai/core').then(module => {
-  console.log('Available exports:');
-  Object.keys(module).forEach(key => {
-    console.log(`- ${key}: ${typeof module[key]}`);
-  });
-}).catch(err => {
-  console.error('Error:', err);
-});
